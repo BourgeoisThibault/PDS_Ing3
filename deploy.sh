4ee56131e295b1126811035ef7e798cbdc139771
BOURGEOIS THIBAULT <thibault.bourgeois@etu.u-pec.fr>
2017-11-13 19:15:59 +0100

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-scp -r -p esibank@192.154.88.151:/builds/esibank-project/project-esibank/notification-server/target/notificationserver.war esibank@192.154.88.151:/home/esibank/app.war
+scp StrictHostKeyChecking=no -r -p esibank@192.154.88.151:/builds/esibank-project/project-esibank/notification-server/target/notificationserver.war esibank@192.154.88.151:/home/esibank/app.war
