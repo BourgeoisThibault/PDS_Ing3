06260bb4f8a69915e44b391ca97ac8878ce22b37
BOURGEOIS THIBAULT <thibault.bourgeois@etu.u-pec.fr>
2017-11-14 14:09:56 +0100

    remove banktransfert because not a module

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sshpass -p 'esibankpds' scp StrictHostKeyChecking=no -r -p /builds/esibank-project/project-esibank/notification-server/target/notificationserver.war esibank@192.154.88.151:/home/esibank/app.war
