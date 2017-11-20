e890ecc1e068991b0e257fc9afcf1cf92f360103
t.bourgeois <thibault.bourgeois@etu.u-pec.fr>
2017-11-20 23:05:30 +0100

    adding rabbitmq

--- notification-server/src/main/java/pds/esibank/notificationserver/controllers/HomeController.java
+++ notification-server/src/main/java/pds/esibank/notificationserver/controllers/HomeController.java
@@ -1,9 +1,13 @@
 package pds.esibank.notificationserver.controllers;
 
 import org.apache.log4j.Logger;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import pds.esibank.models.notification.NotificationModel;
 import pds.esibank.notificationserver.utils.ListOfTokenGenerate;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author BOURGEOIS Thibault
@@ -14,6 +18,13 @@ import pds.esibank.notificationserver.utils.ListOfTokenGenerate;
 public class HomeController {
 
     private final Logger logger = Logger.getLogger(HomeController.class);
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public HomeController(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String getTest(){
@@ -28,6 +39,21 @@ public class HomeController {
     @RequestMapping(path = "/token/{uid}", method = RequestMethod.GET)
     public String genToken(@PathVariable Long uid){
         return ListOfTokenGenerate.generateUidToken(uid);
+    }
+
+    @RequestMapping(path = "/mq", method = RequestMethod.GET)
+    public String sendMessage() {
+
+        AtomicInteger counter = new AtomicInteger();
+        for (int i = 0; i < 10; i++){
+            System.out.println("Sending notification number " + i);
+            NotificationModel notificationModel = new NotificationModel();
+            notificationModel.setTitle("Titre number " + i);
+            notificationModel.setMessage("Message number " + i);
+            rabbitTemplate.convertAndSend(notificationModel);
+        }
+
+        return "Success";
     }
 
 }
