55131498cfa11b349f612cb5504f7a24abe856f8
sarah.allouche <sarah.allouchesarah27@hotmail.fr>
2017-11-27 00:28:09 +0100

    add correction in diagram

--- bank-transfer/src/main/java/Model/AccessDataTransaction.java
+++ bank-transfer/src/main/java/Model/AccessDataTransaction.java
@@ -14,8 +14,8 @@ public class AccessDataTransaction {
 
     public List<Transaction> getDBTransaction() throws IOException {
         //Get Transaction from the model on ArrayList
-        //final String uri = "http://data-access:8080/transaction/allByDate";
-        final String uri = "http://localhost:8080/transaction/allByDate";
+        final String uri = "http://data-access:8080/transaction/allByDate";
+        //final String uri = "http://localhost:8080/transaction/allByDate";
 
         return(new ObjectMapper().readValue(new URL(uri),
                 TypeFactory.defaultInstance().constructCollectionType(List.class, Transaction.class)));
