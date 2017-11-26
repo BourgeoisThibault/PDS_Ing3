42e5caeac1c2200664e9f7e91e28130072692f8d
t.bourgeois <thibault.bourgeois@etu.u-pec.fr>
2017-11-26 03:50:51 +0100

    finalize all it

--- it-notification-send/src/test/java/pds/esibank/it/notification/NotificationTokenTest.java
+++ it-notification-send/src/test/java/pds/esibank/it/notification/NotificationTokenTest.java
@@ -1,3 +1,5 @@
+package pds.esibank.it.notification;
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
@@ -14,5 +16,5 @@ import org.junit.runner.RunWith;
         plugin = {"pretty", "html:target/cucumber" },
         features = "src/main/resources",
         glue = "pds/esibank/it/notification")
-public class NotificationIt {
+public class NotificationTokenTest {
 }
