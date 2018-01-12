5d19a4e78c2bdab94a805a36405245d3dbd6fbc7
Mariam <mariam.haidara@etu.u-pec.fr>
2018-01-12 14:47:29 +0100

    Configuration to create a queue.

--- shares-price-historisation/src/main/java/pds/esibank/historisation/DemoApplication.java
+++ shares-price-historisation/src/main/java/pds/esibank/historisation/DemoApplication.java
@@ -1,12 +1,32 @@
 package pds.esibank.historisation;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pds.esibank.models.shares.Share;
+import pds.esibank.models.shares.SharePrice;
+
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	private static RabbitTemplate rabbitTemplate;
+
+	@Autowired
+	public DemoApplication(RabbitTemplate rabbitTemplate) {
+		this.rabbitTemplate = rabbitTemplate;
+	}
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
+
+		SharePrice sharePrice = new SharePrice();
+		sharePrice.setChange(new BigDecimal(1111));
+		sharePrice.setPrice(new BigDecimal(9999));
+		sharePrice.setShare(new Share());
+		rabbitTemplate.convertAndSend(sharePrice);
 	}
 }
