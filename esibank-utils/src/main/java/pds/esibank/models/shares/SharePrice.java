8de7adc0ff470899842e23b58948c823d5daf7ef
tarik.kich <tarik.kich@hotmail.fr>
2018-02-11 19:45:13 +0100

    Fix lombok Builder  problem

--- esibank-utils/src/main/java/pds/esibank/models/shares/SharePrice.java
+++ esibank-utils/src/main/java/pds/esibank/models/shares/SharePrice.java
@@ -1,29 +1,29 @@
 package pds.esibank.models.shares;
 
-import lombok.Builder;
 import lombok.Data;
+import lombok.Builder;
 import lombok.ToString;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
 @Data
-@Builder
 @ToString
+@Builder
 public class SharePrice {
     private Share share;
     private BigDecimal price;
     private BigDecimal change;
     private Date time;
 
-   /*public SharePrice() {
+    public SharePrice() {
         super();
-   }
+    }
 
-  public SharePrice(String name, String symbol, String currency, BigDecimal price, BigDecimal change, Date time) {
-       this.share = new Share(name, symbol, currency);
-      this.price = price;
-      this.change = change;
-     this.time = time;
-  }*/
+    public SharePrice(Share share, BigDecimal price, BigDecimal change, Date time) {
+        this.share = share;
+        this.price = price;
+        this.change = change;
+        this.time = time;
+    }
 }
