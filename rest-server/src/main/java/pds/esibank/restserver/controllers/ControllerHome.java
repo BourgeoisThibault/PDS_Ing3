f0fc0c5003a2361a4ef6152f6e22b370fc3c1a16
Thibault <thibault.bourgeois@etu.u-pec.fr>
2018-03-07 15:46:17 +0100

    change rest

--- rest-server/src/main/java/pds/esibank/restserver/controllers/ControllerHome.java
+++ rest-server/src/main/java/pds/esibank/restserver/controllers/ControllerHome.java
@@ -1,6 +1,9 @@
 package pds.esibank.restserver.controllers;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @RestController
 public class ControllerHome {
@@ -10,4 +13,15 @@ public class ControllerHome {
         return "<H1>REST ESIBANK HOME NEW</H1>";
     }
 
+    private static final String URI_DATA_ACCESS = "http://10.10.2.1:8080/";
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping("/test")
+    public String test() throws IOException {
+
+        String toto = restTemplate.getForObject(URI_DATA_ACCESS,String.class);
+        return toto;
+    }
+
 }
+
