ae2e0a5b458f169f3d2af213b15def3dc3031032
u.abidbutt <usman.abid.butt@hotmail.com>
2018-03-18 14:24:10 +0100

    UC NFC DAB add accounts repo

---
+++ data-access-server/src/main/java/pds/esibank/dataaccess/repositories/AccountRepository.java
@@ -0,0 +1,21 @@
+package pds.esibank.dataaccess.repositories;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import pds.esibank.dataaccess.entities.Account;
+
+import java.util.List;
+
+public interface AccountRepository extends CrudRepository<Account, Long> {
+
+
+    @Query("select a from account a where account_id = ?1")
+    List<Account> getAccountByAccountId(long account_id);
+
+
+    @Query("select a from account a where card_id_fk = ?1")
+    List<Account> getListAccountByCardId(long card_id);
+
+    List<Account> findAll();
+}
