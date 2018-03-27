84c007e9669f1a111cb5e65b37c243f98f40565c
jeremy.peythieu <peythieu.jeremy@gmail.com>
2018-03-27 22:29:50 +0200

    Add Entities créances clients

---
+++ data-access-server/src/main/java/pds/esibank/dataaccess/entities/Receivable.java
@@ -0,0 +1,25 @@
+package pds.esibank.dataaccess.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * Created by jeremy on 27/03/2018.
+ */
+@Data
+@Entity(name = "receivable")
+public class Receivable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column( name = "receivable_id" )
+    private Long receivable_id;
+    @Column(name = "receivable_amount")
+    private String receivable_type;
+    @Column( name = "receivable_rate" )
+    private String receivable_rate;
+    @Column( name = "receivable_assurance" )
+    private String receivable_assurance;
+    @Column( name = "receivable_duration" )
+    private String receivable_duration;
+}
