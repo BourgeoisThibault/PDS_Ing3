7cca6c8b803ab9aeedb5382004d32456983f7d57
sarah.allouche <sarah.allouchesarah27@hotmail.fr>
2018-04-18 23:16:52 +0200

    add others analysis

--- bigdata_complaintsAnalysis/src/main/java/Main/ComplaintsAnalysis.java
+++ bigdata_complaintsAnalysis/src/main/java/Main/ComplaintsAnalysis.java
@@ -1,15 +1,19 @@
 package Main;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.catalyst.expressions.Year;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.apache.spark.sql.functions.col;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 
 import org.apache.spark.api.java.JavaRDD;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 
 /**
  * Created by SarahAllouche on 16/04/2018.
@@ -41,6 +45,7 @@ public class ComplaintsAnalysis {
         System.out.println("# contains Mortgage: " + jrdd.filter(line -> line.contains("Mortgage") ).count() );
 */
 
+        DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
         SparkSession spark = SparkSession
                 .builder()
                 .appName("Spark Sarah")
@@ -49,13 +54,22 @@ public class ComplaintsAnalysis {
 
         Dataset<Row> ds = spark.read().option("delimiter", ",")
                 .option("header", true)
+                .option("Date_received", "dd/MM/yyyy")
                 .csv("/Users/SarahAllouche/Documents/Consumer_Complaints.csv");
         //Charge le cache
         //ds.cache();
         ds.groupBy("Product").count().show();
+        ds.groupBy("Consumer_disputed").count().show();
+        ds.groupBy("Company_response_to_consumer").count().show();
 
-        double count = ds.count();
-        System.out.println("le count " + count);
+        // double count = ds.filter(to_date(col("Date_received").cast(date_format())).gt(01/01/2016)).count();
+        //ds.select(to_date(col("Date_received"),"yyyy") == 2016);
+       // ds.select(to_date(ds.col("Date_received"),"y")==2010);
+                //Year($"Date").geq(lit(2016)));
+                //data.filter(year($"date").geq(lit(2016)))
+
+        //double count = ds.count();
+        //System.out.println("le count " + count);
 
         //ds.show();
         // decharge le cache
