97985bf60da3c4c597c2276a3e0371a57a000cb1
sarah.allouche <sarah.allouchesarah27@hotmail.fr>
2018-04-18 22:07:35 +0200

    add modifications + add count on type of problems

--- bigdata_complaintsAnalysis/src/main/java/Main/ComplaintsAnalysis.java
+++ bigdata_complaintsAnalysis/src/main/java/Main/ComplaintsAnalysis.java
@@ -1,10 +1,13 @@
 package Main;
 import org.apache.spark.SparkConf;
-import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import scala.Tuple2;
+
+import static org.apache.spark.sql.functions.col;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 
 import org.apache.spark.api.java.JavaRDD;
 
@@ -13,10 +16,12 @@ import org.apache.spark.api.java.JavaRDD;
  */
 public class ComplaintsAnalysis {
 
-    public static void main( String[] args )
+
+
+    public void run()
     {
 
-
+        /*
         //Create a SparkContext to initialize Spark
         SparkConf conf = new SparkConf().setAppName("CountProject").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
@@ -24,16 +29,45 @@ public class ComplaintsAnalysis {
         //Path to replace with csv on server.
         String path = "//file:///Users/SarahAllouche/Documents/Consumer_Complaints.csv";
 
-        JavaRDD<String> jrdd = sc.textFile(path.toString());
+        JavaRDD<String> jrdd = sc.textFile(path);
 
-// Number of lines
+        // Number of lines
         System.out.println("# lines: " + jrdd.count() );
 
-// Number of lines with more than 5 words
+        // Number of lines with Mortgage
        // System.out.println("# with mortgage : " + jrdd.filter(line -> line.split(",").equals("Mortgage") ).count() );
 
-// Number of lines which contains the word "Spark"
+        // Number of lines which contains the word "Spark"
         System.out.println("# contains Mortgage: " + jrdd.filter(line -> line.contains("Mortgage") ).count() );
+*/
+
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Spark Sarah")
+                .master("local[*]")
+                .getOrCreate();
+
+        Dataset<Row> ds = spark.read().option("delimiter", ",")
+                .option("header", true)
+                .csv("/Users/SarahAllouche/Documents/Consumer_Complaints.csv");
+        //Charge le cache
+        //ds.cache();
+        ds.groupBy("Product").count().show();
+
+        double count = ds.count();
+        System.out.println("le count " + count);
+
+        //ds.show();
+        // decharge le cache
+        ds.unpersist();
+    }
+
+
+    public static void main( String[] args )
+    {
+        ComplaintsAnalysis j = new ComplaintsAnalysis();
+        j.run();
+
     }
 
 }
