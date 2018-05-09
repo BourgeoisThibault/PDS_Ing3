a0fbf1460f69eb5ef35bb4d66140b36bea800248
jeremy.peythieu <peythieu.jeremy@gmail.com>
2018-05-09 13:13:36 +0200

    Call Retrieve method

--- sentimentAnalysis/src/main/java/Services/DataProcessing.java
+++ sentimentAnalysis/src/main/java/Services/DataProcessing.java
@@ -1,14 +1,26 @@
 package Services;
 
+import DataDefinition.DataDefinition;
+import org.apache.log4j.Logger;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+
 /**
  * Created by jeremy on 09/05/2018.
  */
 public class DataProcessing {
+    private DataDefinition dataDefiniton = new DataDefinition();
+    private static final Logger logger = Logger.getLogger(DataProcessing.class);
 
     public void run()
     {
+        logger.info("Method run - Start : Sentiment Analysis");
+        Dataset<Row> dataset = dataDefiniton.RetrieveDataFromMongodb();
+        dataset.cache();
+
 
     }
+
 
 
     public static void main( String[] args )
