230cb66aaa0967eb88234b656fd85ac16fad2f4a
jeremy.peythieu <peythieu.jeremy@gmail.com>
2018-05-09 13:05:33 +0200

    Adding Data Access class

---
+++ sentimentAnalysis/src/main/java/Services/DataAccess.java
@@ -0,0 +1,22 @@
+package Services;
+
+/**
+ * Created by jeremy on 09/05/2018.
+ */
+public class DataAccess {
+
+    public void run()
+    {
+
+    }
+
+
+    public static void main( String[] args )
+    {
+        DataAccess SentimentAnalysis = new DataAccess();
+        SentimentAnalysis.run();
+
+    }
+
+
+}
