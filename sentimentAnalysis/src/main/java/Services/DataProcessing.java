82442429a846badcec7f220eb45c9f988291ffab
jeremy.peythieu <peythieu.jeremy@gmail.com>
2018-05-11 01:46:41 +0200

    Update config

--- sentimentAnalysis/src/main/java/Services/DataProcessing.java
+++ sentimentAnalysis/src/main/java/Services/DataProcessing.java
@@ -4,27 +4,40 @@ import DataDefinition.DataDefinition;
 import org.apache.log4j.Logger;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 
 /**
  * Created by jeremy on 09/05/2018.
  */
-@Configuration
+@Component
 public class DataProcessing {
-   // @Value("${url.secure}")
-   // private String url;
+/*
+    public static String DATA_ACCESS_URI;
+    public static String SECURE_LOGIN;
+    public static String SECURE_PASSWORD;
+
+
+    @Value("${uri.secure}")
+    public void setSECURE_URI(String DATA_ACCESS_URI) {
+        this.DATA_ACCESS_URI = DATA_ACCESS_URI;
+    }
 
     @Value("${db.mysql-login}")
-    private String login;
+    public void setSECURE_LOGIN(String SECURE_LOGIN) {
+        this.SECURE_LOGIN = SECURE_LOGIN;
+    }
 
     @Value("${db.mysl-password}")
-    private String password;
+    public void setSECURE_PASSWORD(String SECURE_PASSWORD) {
+        this.SECURE_PASSWORD = SECURE_PASSWORD;
+    }*/
 
     //private String url = "jdbc:mysql://192.154.88.161:3306/esibank_decisionnel";
-    private String url = "jdbc:mysql://localhost:8889/esibank_decisionnel";
+    private String url = "jdbc:mysql://127.0.0.1:8889/esibank_decisionnel";
     private DataAccess dataAccess = new DataAccess(url,"esibank", "esibankpds");
+    //private DataAccess dataAccess = new DataAccess(DATA_ACCESS_URI,SECURE_LOGIN, SECURE_PASSWORD);
+
     private DataDefinition dataDefiniton = new DataDefinition();
     private static final Logger logger = Logger.getLogger(DataProcessing.class);
 
