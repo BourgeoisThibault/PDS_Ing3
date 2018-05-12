14c33d665910425e796cb7a66559c61b992de381
u.abidbutt <usman.abid.butt@hotmail.com>
2018-05-12 11:15:23 +0200

    add CRUD for IT tests cucumber

--- data-access-server/src/main/java/pds/esibank/dataaccess/controllers/AccountController.java
+++ data-access-server/src/main/java/pds/esibank/dataaccess/controllers/AccountController.java
@@ -4,14 +4,15 @@ package pds.esibank.dataaccess.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pds.esibank.dataaccess.entities.Account;
+import pds.esibank.dataaccess.entities.Card;
 import pds.esibank.dataaccess.services.AccountService;
 import pds.esibank.models.dab.AccountDto;
+import pds.esibank.models.dab.CardDto;
 
+import javax.ws.rs.POST;
+import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @RestController
@@ -34,5 +35,62 @@ public class AccountController {
                 return "ok";
     }
 
+
+    @RequestMapping(value="/createAccount", method= RequestMethod.POST, produces = MediaType.APPLICATION_JSON)
+    public ResponseEntity<?> createAccount(
+            @RequestBody AccountDto accountDto
+            ){
+        System.out.println("id account : "+accountDto.getAccount_id());
+        AccountDto a = accountService.createAccount(accountDto);
+        return new ResponseEntity<AccountDto>(a, HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/createCard", method= RequestMethod.POST, produces = MediaType.APPLICATION_JSON)
+    public ResponseEntity<?> createCard(
+            @RequestBody CardDto cardDto
+            ){
+        System.out.println("id account : "+cardDto.getCardNum());
+        CardDto c = accountService.createCard(cardDto);
+        return new ResponseEntity<CardDto>(c, HttpStatus.OK);
+    }
+
+    @GetMapping("/deleteAccount/{account_id}")
+    public ResponseEntity<?> deleteAccount(
+            @PathVariable String account_id){
+        try {
+            String s = accountService.removeAccount(Long.valueOf(account_id));
+            System.out.println(s);
+            return new ResponseEntity<String>(s, HttpStatus.OK);
+        }catch (Exception e){
+
+        }
+        return new ResponseEntity<String>("ko",HttpStatus.CONFLICT);
+    }
+
+    @GetMapping("/deleteLink/{card_num}")
+    public ResponseEntity<?> deleteLink(
+            @PathVariable String card_num){
+        try {
+            String s = accountService.removeLinkAccountToCard(card_num);
+            System.out.println(s);
+            return new ResponseEntity<String>(s, HttpStatus.OK);
+        }catch (Exception e){
+
+        }
+        return new ResponseEntity<String>("ko",HttpStatus.CONFLICT);
+    }
+
+    @GetMapping("/deleteCard/{card_id}")
+    public ResponseEntity<?> deleteCard(
+            @PathVariable String card_id){
+        try {
+            String s = accountService.removeCard(Long.valueOf(card_id));
+            System.out.println(s);
+            return new ResponseEntity<String>(s, HttpStatus.OK);
+        }catch (Exception e){
+
+        }
+        return new ResponseEntity<String>("ko",HttpStatus.CONFLICT);
+    }
 
 }
