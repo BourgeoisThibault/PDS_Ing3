74e1d6bfd52549a7ef6d712774d46ae22755656f
u.abidbutt <usman.abid.butt@hotmail.com>
2018-05-12 11:16:29 +0200

    set dto and add new fields

--- esibank-utils/src/main/java/pds/esibank/models/dab/CardDto.java
+++ esibank-utils/src/main/java/pds/esibank/models/dab/CardDto.java
@@ -1,10 +1,12 @@
 package pds.esibank.models.dab;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
 
 @Data
 @ToString
+@Builder
 public class CardDto {
         private Long card_id;
         private String cardNum;
