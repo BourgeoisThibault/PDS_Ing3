f316f89fd1e4bf345bf42cc6d0fdd6832e9c5489
u.abidbutt <usman.abid.butt@hotmail.com>
2018-05-12 15:43:25 +0200

    add setter and getters

--- esibank-utils/src/main/java/pds/esibank/models/dab/CardDto.java
+++ esibank-utils/src/main/java/pds/esibank/models/dab/CardDto.java
@@ -6,9 +6,38 @@ import lombok.ToString;
 
 @Data
 @ToString
-@Builder
 public class CardDto {
         private Long card_id;
         private String cardNum;
+
+        public Long getCard_id() {
+                return card_id;
+        }
+
+        public void setCard_id(Long card_id) {
+                this.card_id = card_id;
+        }
+
+        public String getCardNum() {
+                return cardNum;
+        }
+
+        public void setCardNum(String cardNum) {
+                this.cardNum = cardNum;
+        }
+
+        public String getCardPass() {
+                return cardPass;
+        }
+
+        public void setCardPass(String cardPass) {
+                this.cardPass = cardPass;
+        }
+
         private String cardPass;
+
+        public CardDto() {
+                super();
+        }
+
 }
