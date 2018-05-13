70adb1ccf7375de85683d944a7ca9238862bbf82
jeremy.peythieu <peythieu.jeremy@gmail.com>
2018-05-13 03:24:54 +0200

    Adding methods for data processing from a CSV file

--- sentimentAnalysis/src/main/java/DataDefinition/DataDefinition.java
+++ sentimentAnalysis/src/main/java/DataDefinition/DataDefinition.java
@@ -6,6 +6,10 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 
 /**
  * Created by jeremy on 09/05/2018.
@@ -36,6 +40,31 @@ public class DataDefinition {
     {
         logger.info("Retrieve multiple data from Mongodb into Dataset");
         return MongoSpark.load(jsc).toDF();
+    }
+
+    public Dataset<Row> RetrieveDataFromCSV(final String path, final StructType schemaCsv){
+        logger.info("Method RetrieveDataFromCSV - Start");
+
+        return spark.read()
+                .option("delimiter", ",")
+                .option("inferSchema", true)
+                .option("multiline", true)
+                .option("header", true)
+                .schema(schemaCsv)
+                .csv(path);
+
+
+    }
+    public StructType getSchema()
+    {
+        logger.info("Method getSchema - Start : Get schema");
+
+        return new StructType(new StructField[]{
+                new StructField("id", DataTypes.LongType, false, Metadata.empty()),
+                new StructField("date", DataTypes.DateType, false, Metadata.empty()),
+                new StructField("note", DataTypes.IntegerType, false, Metadata.empty()),
+                new StructField("commentaire", DataTypes.StringType, false, Metadata.empty())
+        });
     }
 
 }
