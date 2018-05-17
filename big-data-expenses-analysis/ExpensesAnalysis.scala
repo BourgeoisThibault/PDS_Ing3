63d86984447b0807ea231c7561a7639bba77b7ed
tarik.kich <tarik.kich@hotmail.fr>
2018-05-17 20:37:07 +0200

    optim : connect to mongodb instead of csv

--- big-data-expenses-analysis/ExpensesAnalysis.scala
+++ big-data-expenses-analysis/ExpensesAnalysis.scala
@@ -1,8 +1,12 @@
-val dfTransactions=sqlContext.read.format("csv").option("header", "false").load("/home/tarik/transactions")
-val dfClientCards=sqlContext.read.format("csv").option("header", "false").load("/home/tarik/cards")
+import com.mongodb.spark._
+import com.mongodb.spark.config._
+
+val cl_rdd = sc.loadFromMongoDB(ReadConfig(Map("uri" -> "mongodb://127.0.0.1/pds_db.clients")))
+val tr_rdd = sc.loadFromMongoDB(ReadConfig(Map("uri" -> "mongodb://127.0.0.1/pds_db.transactions")))
+
 val dfCardTypes=sqlContext.read.format("csv").option("header", "false").load("/home/tarik/card_types")
 
-val dfCardTransactions = dfTransactions.filter(dfTransactions("type") === "CB")
+val dfCardTransactions = dfTransactions.filter(dfTransactions("card_type") === "CB")
 
 val dfTmp = dfCardTransactions.join(dfClientCards, dfCardTransactions.col("account_id") === dfClientCards.col("account_id"))
 
