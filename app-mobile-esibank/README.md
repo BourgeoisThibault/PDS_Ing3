9060720dd05361d1c13f26e9a5b022ecdc6589c9
BOURGEOIS THIBAULT <thibault.bourgeois@etu.u-pec.fr>
2018-05-24 16:21:59 +0200

    Suppression du travail des autres membres

--- app-mobile-esibank/README.md
+++
@@ -1,8 +0,0 @@
-US ESIBANK 14 : Developement of notification service on android device
-
-- Based on websocket communication 
-- Supports fault-tolerance for certain notifications
-- Supports Auto Switch Between Wi-Fi & Mobile Data Networks
-- Pushes notifications (from server) to a native Android application
-- Uses JSON format for communication
-- ...
--- big-data-expenses-analysis/launch.sh
+++
@@ -1,5 +0,0 @@
-./spark-shell --packages com.databricks:spark-csv_2.10:1.4.0 \
-			--conf "spark.mongodb.input.uri=mongodb://127.0.0.1/pds_db.clients?readPreference=primaryPreferred" \
-			--conf "spark.mongodb.output.uri=mongodb://127.0.0.1/pds_db.clients" \
-			--packages org.mongodb.spark:mongo-spark-connector_2.11:2.2.2 \
-			-i /ExpensesAnalysis.scala
